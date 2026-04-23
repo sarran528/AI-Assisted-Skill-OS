61fb5def21fb9a643075862a1c36e57e64cc271c
sarranm528 <sarranm528@gmail.com>
2026-04-23 03:26:45 +0000

    ..

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ def create_app() -> FastAPI:
     app.add_middleware(SlowAPIMiddleware)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://localhost:5173"],
+        allow_origins=["http://localhost:3000"],
         allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allow_headers=["Authorization", "Content-Type", "X-Requested-With"],
         allow_credentials=True,
